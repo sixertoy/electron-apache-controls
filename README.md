d602f5e7167519d47d86ea06c80c124c0099c794
sixertoy <sixertoy.github@gmail.com>
2018-04-14 05:58:03 +0200

    📚 🍿  pdate documentationn, remove dead code, add prettier...

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Electron Apache Controls
 
-**Build your own**
+## Install
+
+```bash
+git clone https://github.com/sixertoy/electron-apache-controls.git
+yarn install
+```
+## Run in dev mode
+
+```bash
+yarn dev
+```
+
+## Build your own
 
 ```bash
 git clone https://github.com/sixertoy/electron-apache-controls.git
@@ -8,3 +20,15 @@ yarn install
 yarn build
 yarn compile
 ```
+
+## Issues
+
+- [x] MacOS only
+- [x] Old School thang
+- [x] No Tests
+- [x] No Debug mode / Error(s) report
+
+### Real Issues
+
+- [ ] No Debug mode / Error(s) report
+- [ ] Error when canceling root prompt
--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-/* eslint
-  no-console: 0,
-  global-require: 0 */
 const sudo = require('sudo-prompt');
 const electron = require('electron');
 const childprocess = require('child_process');
@@ -14,6 +11,8 @@ let tray = null;
 let stopitem = null;
 let startitem = null;
 let contextMenu = null;
+let statuslabel = null;
+let restartitem = null;
 // let installitem = null;
 // let uninstallitem = null;
 
@@ -23,15 +22,48 @@ const icons = {
   stopped: getasset('tray-icon_stopped.png'),
 };
 
-const serviceFile = '/System/Library/LaunchDaemons/org.apache.httpd.plist';
+// const serviceFile = '/System/Library/LaunchDaemons/org.apache.httpd.plist';
 const commands = {
   sudo: {
-    status: ['-k', '$MODE'],
-    service: ['$MODE', '-w', serviceFile],
+    stop: 'apachectl -k stop',
+    start: 'apachectl -k start',
+    restart: 'apachectl -k restart',
+    // install: `launchctl load -w ${serviceFile}`,
+    // uninstall: `launchctl unload -w ${serviceFile}`,
   },
   nosudo: {
     isrunning: 'ps -ax | grep /usr/sbin/httpd',
-    isinstalled: 'launchctl list | grep apache',
+    // isinstalled: 'launchctl list | grep apache',
+  },
+};
+
+const states = {
+  stopped: () => {
+    stopitem.visible = false;
+    startitem.visible = true;
+    restartitem.enabled = false;
+    tray.setImage(icons.stopped);
+  },
+
+  started: () => {
+    stopitem.visible = true;
+    startitem.visible = false;
+    restartitem.enabled = true;
+    tray.setImage(icons.started);
+  },
+
+  idleing: () => {
+    stopitem.visible = false;
+    startitem.visible = false;
+    restartitem.enabled = false;
+    tray.setImage(icons.idle);
+  },
+
+  errored: () => {
+    stopitem.visible = false;
+    startitem.visible = false;
+    restartitem.enabled = false;
+    tray.setImage(icons.idle);
   },
 };
 
@@ -47,49 +79,51 @@ const commands = {
 //     );
 //   });
 
-const isRunning = () =>
-  new Promise((resolve, reject) => {
-    childprocess.exec(
-      // looking for httpd processes
-      commands.nosudo.isrunning,
-      (err, stdout, stderr) => {
-        if (err) {
-          // FIXME -> add debug
-          process.stdout.write(`Apache isRunning() error: ${stderr}`);
-          return reject(stderr);
-        }
-        let pids = (stdout && stdout.split('\n')) || [];
-        // filtering process with grep as a command
-        pids = pids.filter(str => str && str.indexOf('grep') < 0);
-        return resolve(pids.length > 0);
-      },
-      { encoding: 'utf-8' },
-    );
-  });
-
 function stopService () {
-  const opts = { icns: getasset('app-icon.png'), name: app.getName(), encoding: 'utf-8' };
-  sudo.exec('apachectl -k stop', opts, (err, stdout, stderrr) => {
+  const opts = {
+    encoding: 'utf-8',
+    name: app.getName(),
+    icns: getasset('app-icon.png'),
+  };
+  states.idleing();
+  sudo.exec(commands.sudo.stop, opts, (err, stdout, stderrr) => {
     if (err) {
       process.stdout.write(`Stop service error: ${stderrr}`);
       return;
     }
-    stopitem.visible = false;
-    startitem.visible = true;
-    tray.setImage(icons.stopped);
+    states.stopped();
   });
 }
 
 function startService () {
-  const opts = { icns: getasset('app-icon.png'), name: app.getName(), encoding: 'utf-8' };
-  sudo.exec('apachectl -k start', opts, (err, stdout, stderrr) => {
+  const opts = {
+    encoding: 'utf-8',
+    name: app.getName(),
+    icns: getasset('app-icon.png'),
+  };
+  states.idleing();
+  sudo.exec(commands.sudo.start, opts, (err, stdout, stderrr) => {
     if (err) {
       process.stdout.write(`Start service error: ${stderrr}`);
       return;
     }
-    stopitem.visible = true;
-    startitem.visible = false;
-    tray.setImage(icons.started);
+    states.started();
+  });
+}
+
+function restartService () {
+  const opts = {
+    encoding: 'utf-8',
+    name: app.getName(),
+    icns: getasset('app-icon.png'),
+  };
+  states.idleing();
+  sudo.exec(commands.sudo.restart, opts, (err, stdout, stderrr) => {
+    if (err) {
+      process.stdout.write(`Restart service error: ${stderrr}`);
+      return;
+    }
+    states.started();
   });
 }
 
@@ -123,22 +157,48 @@ function startService () {
 //   });
 // }
 
+const isRunning = () =>
+  new Promise((resolve, reject) => {
+    childprocess.exec(
+      // looking for httpd processes
+      commands.nosudo.isrunning,
+      (err, stdout, stderr) => {
+        if (err) {
+          // FIXME -> add debug
+          process.stdout.write(`Apache isRunning() error: ${stderr}`);
+          reject(stderr);
+          return;
+        }
+        let pids = (stdout && stdout.split('\n')) || [];
+        // filtering process with grep as a command
+        pids = pids.filter(str => str && str.indexOf('grep') < 0);
+        resolve(pids.length > 0);
+      },
+      { encoding: 'utf-8' },
+    );
+  });
+
 function initMenu () {
   stopitem = contextMenu.getMenuItemById('stop-controller');
   startitem = contextMenu.getMenuItemById('start-controller');
+  restartitem = contextMenu.getMenuItemById('restart-controller');
+  statuslabel = contextMenu.getMenuItemById('status-controller-label');
   isRunning()
     .then((running) => {
-      console.log('running', running);
-      stopitem.visible = running;
-      startitem.visible = !running;
+      restartitem.visible = true;
+      statuslabel.visible = true;
+      if (running) states.started();
+      else states.stopped();
       tray.setContextMenu(contextMenu);
-      tray.setImage(icons[running ? 'started' : 'stopped']);
     })
     .catch(() => {
-      stopitem.visible = false;
-      startitem.visible = false;
+      states.errored();
       tray.setContextMenu(contextMenu);
     });
+  // FAIL :(
+  // .finally(() => {
+  //   tray.setContextMenu(contextMenu);
+  // });
 }
 
 // quand l'user click sur l'icone dans le dock
@@ -146,14 +206,15 @@ function initMenu () {
 app.on('window-all-closed', () => (!isdarwin() ? app.quit() : noop));
 app.on('ready', () => {
   app.dock.hide();
-  // const mainWindow = new BrowserWindow();
-  // mainWindow.setSkipTaskbar(true);
   tray = new Tray(icons.idle);
+  // tray.setTitle(`${app.getName()} v${app.getVersion()}`);
   tray.setToolTip(`${app.getName()} v${app.getVersion()}`);
   contextMenu = Menu.buildFromTemplate([
     {
       enabled: false,
+      visible: false,
       label: 'Server status',
+      id: 'status-controller-label',
     },
     {
       visible: false,
@@ -168,10 +229,21 @@ app.on('ready', () => {
       id: 'start-controller',
     },
     { type: 'separator' },
+    {
+      enabled: false,
+      visible: false,
+      click: restartService,
+      label: 'Restart server',
+      id: 'restart-controller',
+    },
+    { type: 'separator' },
     { label: `About ${app.getName()}`, role: 'about' },
     { label: `Version ${app.getVersion()}`, enabled: false },
+    // { type: 'separator' },
+    // { label: 'Dev menu', enabled: false },
+    // { label: 'Reload', role: 'forcereload' },
     { type: 'separator' },
-    { label: 'Quitter', type: 'normal', role: 'quit' },
+    { label: 'Quitter', role: 'quit' },
   ]);
   tray.setContextMenu(contextMenu);
   initMenu();
